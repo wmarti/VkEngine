5f5790d1cd2fdea2271b3d5c92883ff0a453a7b4
Will Martin <wmarti@bu.edu>
2022-05-05 22:20:15 -0400

    deleted build folder

--- build/external/SDL2/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/wmarti/VkEngine@5677ded7048f46a6ee314e62ca67c8ea3ff17e74"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
